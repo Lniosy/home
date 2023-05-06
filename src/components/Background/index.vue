b5a70acfb85c0a70dc3272fc76c955224bac5d83
Lniosy <50223897@qq.com>
2023-05-06 21:48:23 +0800

    去除随机风景壁纸（api加载太慢容易卡死）

--- src/components/Background/index.vue
+++ src/components/Background/index.vue
@@ -30,8 +30,6 @@ const changeBg = (type) => {
   } else if (type == 1) {
     bgUrl.value = "https://api.dujin.org/bing/1920.php";
   } else if (type == 2) {
-    bgUrl.value = "https://api.btstu.cn/sjbz/api.php?lx=fengjing&format=images";
-  } else if (type == 3) {
     bgUrl.value = "https://www.dmoe.cc/random.php";
   }
 };
